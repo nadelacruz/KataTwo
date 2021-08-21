f11f4902a7aaac1199f7b3f7f47b96ba273f6b84
Nico Dela Cruz <nadelacruz@github.com>
2021-08-21 11:15:22 +0800

    feat: Returns 0 if input array has only one element.

--- src/BinaryChop.java
+++ src/BinaryChop.java
@@ -3,6 +3,12 @@ public class BinaryChop {
         if (arrayOfNumbers.length == 0) {
             return -1;
         }
+        if (arrayOfNumbers.length == 1) {
+            if (arrayOfNumbers[0] != numberToSearch) {
+                return -1;
+            }
+            return 0;
+        }
         return -99;
     }
 }
